4c20ea58eec35adc1a5c30287ca2e38c7b03361e
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-12 06:40:00 +0200

    chore: refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Sonix generic PipeWire echo/noise processing with GoXLR routing";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
+    rs-harbor.url = "git+https://codefloe.com/caniko/rs-harbor.git?ref=trunk&rev=7fa1c2104dab4e1dbaa1aaa6df84bba815aa282d";
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
     crane.follows = "rs-harbor/crane";
@@ -53,10 +53,13 @@
         namespaceScope = "canix-rust";
         namespaceGeneration = 5;
       };
+      cargoArtifacts = buildCache.withRustCache {
+        package = craneLib.buildDepsOnly commonArgs;
+      };
       package = buildCache.withRustCache {
         package = craneLib.buildPackage (commonArgs
           // {
-            cargoArtifacts = null;
+            inherit cargoArtifacts;
             cargoExtraArgs = "--locked --bins";
           });
       };
